71019c6f36690ec42154bd0166889af2f26ad97e
YOenterprise6 <101932043+YOenterprise6@users.noreply.github.com>
2022-03-21 09:30:06 +0100

    Add files via upload

---
+++ Minewatch/Minewatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minewatch
+//
+//  Created by Yoann Dornier on 20/03/2022.
+//  Copyright © 2022 Yoann Dornier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Minewatch/YOvideo/YOvideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YOvideo
+//
+//  Created by Yoann Dornier on 19/03/2022.
+//  Copyright © 2022 Yoann Dornier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
